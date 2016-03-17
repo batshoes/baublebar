19dbe2a86f6b8d9aff3dce69c1b6fde22b667984
James Middlemiss <middlemissj.usa@gmail.com>
2016-03-17 13:40:23 -0400

    adds tickets controller?

--- lib/baublebar.rb
+++ lib/baublebar.rb
@@ -1,14 +1,15 @@
 require "baublebar/version"
 require_relative 'zendesk_call'
+class TicketsController
+  module Baublebar
+    class GetZenDeskTickets
 
-module Baublebar
-  class GetZenDeskTickets
-
-    def get_tickets(email)
-      @user = GetUserRequest.new.call(email)
-      @user_id = @user['id']
-      @tickets = GetTicketRequest.new.call(email, @user_id)
-      @tickets
+      def get_tickets(email)
+        @user = GetUserRequest.new.call(email)
+        @user_id = @user['id']
+        @tickets = GetTicketRequest.new.call(email, @user_id)
+        @tickets
+      end
     end
   end
 end
